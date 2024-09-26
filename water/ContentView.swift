22124482bc3c0fb11d2e4ce1c6734032bf28aabf
Renad <uniirena@gmail.com>
2024-09-26 17:39:24 +0300

    a change in the ui

--- water/ContentView.swift
+++ water/ContentView.swift
@@ -4,16 +4,31 @@
 //
 //  Created by Renad Alyousef on 23/03/1446 AH.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("Water Tracker 💦")
+                .fontWeight(.bold)
+            .font(.largeTitle)}
+        HStack{
+            
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Text("    Apple health")
+            }
+        }
+        
+        HStack{
+       
+Stepper   (value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(4)/*@END_MENU_TOKEN@*/, in: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Range@*/1...10/*@END_MENU_TOKEN@*/) {
+    Text("    Cups to drink per day 0")
+                    }
+        }
+        
+        Button("Continue") {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
         }
         .padding()
     }
