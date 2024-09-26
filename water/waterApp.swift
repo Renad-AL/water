f2f78ea1191fb1623acdd3286340ff2a1c07f9ec
Renad <uniirena@gmail.com>
2024-09-26 17:33:59 +0300

    Initial Commit

---
+++ water/waterApp.swift
@@ -0,0 +1,17 @@
+//
+//  waterApp.swift
+//  water
+//
+//  Created by Renad Alyousef on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct waterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ waterTests/waterTests.swift
@@ -0,0 +1,17 @@
+//
+//  waterTests.swift
+//  waterTests
+//
+//  Created by Renad Alyousef on 23/03/1446 AH.
+//
+
+import Testing
+@testable import water
+
+struct waterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
